d1791e2af99fe17997078453b96af7e36850c3c9
JM <georgemares14@yahoo.com>
2022-09-30 22:58:24 -0700

    Added working timer and did some bug fixing

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const startBtn = document.querySelector("#start-button");
-let time = document.querySelector("#time-value");
-const quizContainer = document.querySelector("#quiz")
+const quizContainer = document.querySelector("#quiz");
 startBtn.addEventListener("click", question1);
 
+function startTimer() {
+  let time = document.getElementById("time-value");
+  let timer = time.textContent - 1;
+  time.innerHTML = timer;
+};
+
 function wrongAnswer() {
-  time.value -5
+  let time = document.getElementById("time-value");
+  time.innerHTML = time.textContent - 10;
+};
+
+function saveScore() {
+  
 }
 
 function question1() {
+  window.startCountdown = setInterval(startTimer, 1000);
+
   quizContainer.innerHTML = `
     <h1 class="text-center">Commonly used data types DO NOT include:</h1>
     <ul class="">
@@ -16,11 +28,10 @@ function question1() {
             <li><a class="btn btn-primary btn-lg mt-5 ml-5" id="answer3" href="#" role="button">3. alerts</a></li>
             <li><a class="btn btn-primary btn-lg mt-5 ml-5" id="answer4" href="#" role="button">4. numbers</a></li>
         </ul>`;
-    document.querySelector("#answer1").addEventListener("click", wrongAnswer)
-    document.querySelector("#answer2").addEventListener("click", wrongAnswer)
-    document.querySelector("#answer3").addEventListener("click", question2)
-    document.querySelector("#answer4").addEventListener("click", wrongAnswer)
-  
+  document.querySelector("#answer1").addEventListener("click", wrongAnswer);
+  document.querySelector("#answer2").addEventListener("click", wrongAnswer);
+  document.querySelector("#answer3").addEventListener("click", question2);
+  document.querySelector("#answer4").addEventListener("click", wrongAnswer);
 };
 
 function question2() {
@@ -32,10 +43,10 @@ function question2() {
             <li><a class="btn btn-primary btn-lg mt-5 ml-5" id="answer3" href="#" role="button">3. parenthesis</a></li>
             <li><a class="btn btn-primary btn-lg mt-5 ml-5" id="answer4" href="#" role="button">4. square brackets</a></li>
         </ul>`;
-    document.querySelector("#answer1").addEventListener("click", wrongAnswer)
-    document.querySelector("#answer2").addEventListener("click", wrongAnswer)
-    document.querySelector("#answer3").addEventListener("click", question3)
-    document.querySelector("#answer4").addEventListener("click", wrongAnswer)
+  document.querySelector("#answer1").addEventListener("click", wrongAnswer);
+  document.querySelector("#answer2").addEventListener("click", wrongAnswer);
+  document.querySelector("#answer3").addEventListener("click", question3);
+  document.querySelector("#answer4").addEventListener("click", wrongAnswer);
 };
 
 function question3() {
@@ -47,13 +58,13 @@ function question3() {
             <li><a class="btn btn-primary btn-lg mt-5 ml-5" id="answer3" href="#" role="button">3. booleans</a></li>
             <li><a class="btn btn-primary btn-lg mt-5 ml-5" id="answer4" href="#" role="button">4. all of the above</a></li>
         </ul>`;
-    document.querySelector("#answer1").addEventListener("click", wrongAnswer)
-    document.querySelector("#answer2").addEventListener("click", wrongAnswer)
-    document.querySelector("#answer3").addEventListener("click", wrongAnswer)
-    document.querySelector("#answer4").addEventListener("click", question4)
+  document.querySelector("#answer1").addEventListener("click", wrongAnswer);
+  document.querySelector("#answer2").addEventListener("click", wrongAnswer);
+  document.querySelector("#answer3").addEventListener("click", wrongAnswer);
+  document.querySelector("#answer4").addEventListener("click", question4);
 };
 
-function question4 () {
+function question4() {
   quizContainer.innerHTML = `
     <h1 class="text-center">A very useful tool used during development and debugging for printing content to the debugger is:</h1>
     <ul class="">
@@ -62,13 +73,13 @@ function question4 () {
             <li><a class="btn btn-primary btn-lg mt-5 ml-5" id="answer3" href="#" role="button">3. for loops</a></li>
             <li><a class="btn btn-primary btn-lg mt-5 ml-5" id="answer4" href="#" role="button">4. console.log</a></li>
         </ul>`;
-    document.querySelector("#answer1").addEventListener("click", wrongAnswer)
-    document.querySelector("#answer2").addEventListener("click", wrongAnswer)
-    document.querySelector("#answer3").addEventListener("click", wrongAnswer)
-    document.querySelector("#answer4").addEventListener("click", question5)
-}
+  document.querySelector("#answer1").addEventListener("click", wrongAnswer);
+  document.querySelector("#answer2").addEventListener("click", wrongAnswer);
+  document.querySelector("#answer3").addEventListener("click", wrongAnswer);
+  document.querySelector("#answer4").addEventListener("click", question5);
+};
 
-function question5 () {
+function question5() {
   quizContainer.innerHTML = `
     <h1 class="text-center">String values must be enclosed within _____ when being assigned to variables</h1>
     <ul class="">
@@ -77,15 +88,33 @@ function question5 () {
             <li><a class="btn btn-primary btn-lg mt-5 ml-5" id="answer3" href="#" role="button">3. quotes</a></li>
             <li><a class="btn btn-primary btn-lg mt-5 ml-5" id="answer4" href="#" role="button">4. parenthesis</a></li>
         </ul>`;
-    document.querySelector("#answer1").addEventListener("click", wrongAnswer)
-    document.querySelector("#answer2").addEventListener("click", wrongAnswer)
-    document.querySelector("#answer3").addEventListener("click", submitScore)
-    document.querySelector("#answer4").addEventListener("click", wrongAnswer)
+  document.querySelector("#answer1").addEventListener("click", wrongAnswer);
+  document.querySelector("#answer2").addEventListener("click", wrongAnswer);
+  document.querySelector("#answer3").addEventListener("click", submitScore);
+  document.querySelector("#answer4").addEventListener("click", wrongAnswer);
 };
 
 function submitScore() {
-  
-}
+  clearInterval(startCountdown);
+  const timeValue = document.querySelector("#time-value").textContent;
+  quizContainer.innerHTML = `
+  <h1 class="text-center"> Well Done!</h1>
+  <h4> Your final Score is: ${timeValue}</h4>
+  <form class="form-inline">
+  <div class="form-group mb-2">
+    <label for="staticEmail2" class="sr-only">Email</label>
+    <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="Enter Initials">
+  </div>
+  <div class="form-group mx-sm-3 mb-2">
+    <label for="inputPassword2" class="sr-only">Password</label>
+    <input type="password" class="form-control" id="inputPassword2" placeholder="Initials">
+  </div>
+  <button type="submit" id="submit-score" class="btn btn-primary mb-2">Submit Score</button>
+</form>
+  `;
+
+  document.querySelector("#submit-score").addEventListener("click",saveScore)
+};
 /*<h1 class="d-flex justify-content-center">Coding Quiz Challenge</h1>
  <span class="d-flex justify-content-center lead"><strong>Try to answer the following code-related questions within the time limit.<br> Keep in mind that incorrect answers will penalize your score/time by<br> ten seconds</strong></span>
  <div class="d-flex justify-content-center mt-5">
